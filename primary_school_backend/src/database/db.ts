bd00f9ff2d73df011359ffdbbd09ec99481cb432
Carlos Maina Wanjiku <softwarewebdevelopers1@gmail.com>
2026-04-27 11:42:43 +0300

    improved graceful shutdown

--- primary_school_backend/src/database/db.ts
+++ primary_school_backend/src/database/db.ts
@@ -4,10 +4,10 @@ const dbConnection = async (): Promise<void> => {
   let connection = await mongoose
     .connect(DotEnvFile.DatabaseConnectionString)
     .then(() => {
-      
+      console.log("Database connected successfully");
     })
     .catch((err) => {
-      
+      console.log("Database connection failed")
     });
 };
 export default dbConnection;
--- primary_school_backend/src/server.ts
+++ primary_school_backend/src/server.ts
@@ -6,20 +6,22 @@ import { newStudent, newStaff } from "./data/seed.js";
 import userRoutes from "./routes/users.js";
 import schoolRoutes from "./routes/school.js";
 import marksRoutes from "./routes/marks.js";
+import mongoose from "mongoose";
 
 let app = express();
 
-app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174"],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true // If you're using cookies/sessions
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:5174"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true, // If you're using cookies/sessions
+  }),
+);
 app.use(express.json());
 
 // Simple request logger
 app.use((req, res, next) => {
-
   next();
 });
 
@@ -32,11 +34,22 @@ app.use("/api/school", schoolRoutes);
 app.use("/api/marks", marksRoutes);
 
 app.listen(DotEnvFile.DEVPort, (): void => {
-
+  console.log(`Server is running on port ${DotEnvFile.DEVPort}`);
 });
 
-function GracefulShutdown(): void {
-
+function GracefulShutdown() {
+  console.log("Shutting down gracefully...");
+  mongoose.connection
+    .close()
+    .then(() => {
+      console.log("Database connection closed.");
+    })
+    .catch((err) => {
+      console.error("Error closing database connection:", err);
+    })
+    .finally(() => {
+      process.exit(0);
+    });
 }
 // signal of interruption
 process.on("SIGINT", GracefulShutdown);
