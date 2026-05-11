cae51a0ae9249383ead7f11b5811fe1f5dd3f48b
Carlos Maina Wanjiku <softwarewebdevelopers1@gmail.com>
2026-05-11 18:01:44 +0300

    PWA installation configs

---
+++ primary_school/react-app/public/sw.js
@@ -0,0 +1,29 @@
+// Simple Service Worker for PWA
+const CACHE_NAME = 'primary-school-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/primary_icon.jpeg',
+  '/Icon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
